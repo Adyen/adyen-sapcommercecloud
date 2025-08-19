2710e9e5e741d5c1f174f76f23d174fcf2767e76
PJaneta <p.janeta@reply.com>
2025-08-19 14:16:23 +0200

    AD-380 Create logic that retrieve original IP address from headers leaved by proxy servers like Akami, Apache etc...

---
+++ adyenv6core/src/com/adyen/v6/service/DefaultAdyenShopperIpResolverService.java
@@ -0,0 +1,56 @@
+package com.adyen.v6.service;
+
+import de.hybris.platform.servicelayer.config.ConfigurationService;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+public class DefaultAdyenShopperIpResolverService implements AdyenShopperIpResolverService {
+    private static final org.apache.log4j.Logger LOG = Logger.getLogger(DefaultAdyenShopperIpResolverService.class);
+
+    static final String X_REAL_IP = "X-Real-IP";
+    static final String CF_CONNECTING_ip = "CF-Connecting-IP";
+    static final String TRUE_CLIENT_IP = "True-Client-IP";
+    static final String FASTLY_CLIENT_IP = "Fastly-Client-IP";
+    static final String X_FORWARDED_FOR = "X-Forwarded-For";
+
+    static final String CUSTOM_HEADER_PROPERTIES_KEY = "adyen.checkout.shopperIpHeader";
+
+    private static final List<String> ONE_VALUE_HEADERS = List.of(X_REAL_IP, CF_CONNECTING_ip, TRUE_CLIENT_IP, FASTLY_CLIENT_IP);
+
+    private ConfigurationService configurationService;
+
+    public String resolveShopperIp(HttpServletRequest request) {
+        String customShopperIpHeader = configurationService.getConfiguration().getString(CUSTOM_HEADER_PROPERTIES_KEY);
+
+        if (StringUtils.isNotEmpty(customShopperIpHeader)) {
+            String headerValue = request.getHeader(customShopperIpHeader);
+
+            LOG.debug("Using custom shopper ip header: " + customShopperIpHeader + " with value: " + headerValue);
+
+            return headerValue;
+        }
+
+        for (String header : ONE_VALUE_HEADERS) {
+            String headerValue = request.getHeader(header);
+            if (StringUtils.isNotEmpty(headerValue)) {
+                LOG.debug("Using header: " + header + " with value: " + headerValue);
+
+                return headerValue;
+            }
+        }
+
+        String headerValue = request.getHeader(X_FORWARDED_FOR);
+        if (StringUtils.isNotEmpty(headerValue)) {
+            return headerValue.split(",")[0].trim();
+        }
+
+        return request.getRemoteAddr();
+    }
+
+    public void setConfigurationService(ConfigurationService configurationService) {
+        this.configurationService = configurationService;
+    }
+}
