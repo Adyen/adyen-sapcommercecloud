7d0bd2e6f000bc223be1dbf7288225b62d9a7fd5
Lukasz756 <lgasiorowski0000@gmail.com>
2025-08-27 11:40:48 +0200

    ADPA-252 Deleted unnecesary comments and files.

--- adyensapdigitalpaymentbackoffice/src/com/adyen/sapdigitalpaymentbackoffice/constants/AdyensapdigitalpaymentbackofficeConstants.java
+++ adyensapdigitalpaymentbackoffice/src/com/adyen/sapdigitalpaymentbackoffice/constants/AdyensapdigitalpaymentbackofficeConstants.java
@@ -1,17 +1,11 @@
-/*
- * Copyright (c) 2022 SAP SE or an SAP affiliate company. All rights reserved
- */
 package com.adyen.sapdigitalpaymentbackoffice.constants;
 
-@SuppressWarnings({"deprecation","squid:CallToDeprecatedMethod"})
-public final class AdyensapdigitalpaymentbackofficeConstants extends GeneratedAdyensapdigitalpaymentbackofficeConstants
-{
+@SuppressWarnings({"deprecation", "squid:CallToDeprecatedMethod"})
+public final class AdyensapdigitalpaymentbackofficeConstants extends GeneratedAdyensapdigitalpaymentbackofficeConstants {
 	public static final String EXTENSIONNAME = "adyensapdigitalpaymentbackoffice";
-	
-	private AdyensapdigitalpaymentbackofficeConstants()
-	{
-		//empty
+
+	private AdyensapdigitalpaymentbackofficeConstants() {
 	}
-	
-	
+
+
 }
