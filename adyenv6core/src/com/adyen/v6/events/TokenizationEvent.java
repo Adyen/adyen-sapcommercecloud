504021a685c7c46d97d72e862a67a44ca547b082
PJaneta <p.janeta@reply.com>
2025-10-10 15:00:43 +0200

    AD-368 Tokenize LPM’s

--- adyenv6core/src/com/adyen/v6/events/TokenizationEvent.java
+++ adyenv6core/src/com/adyen/v6/events/TokenizationEvent.java
@@ -7,7 +7,17 @@ public class TokenizationEvent extends AbstractEvent {
     private final TokenWebhookRequestData data;
 
     public TokenizationEvent(final TokenWebhookRequestData data) {
-        this.data = data;
+        this.data = new TokenWebhookRequestData();
+
+        this.data.setEventId(data.getEventId());
+        this.data.setEventType(data.getEventType());
+        this.data.setOperation(data.getOperation());
+        this.data.setEnvironment(data.getEnvironment());
+        this.data.setShopperReference(data.getShopperReference());
+        this.data.setStoredPaymentMethodId(data.getStoredPaymentMethodId());
+        this.data.setMerchantAccount(data.getMerchantAccount());
+        this.data.setCreatedAt(data.getCreatedAt());
+        this.data.setPaymentType(data.getPaymentType());
     }
 
     public TokenWebhookRequestData getData() {
