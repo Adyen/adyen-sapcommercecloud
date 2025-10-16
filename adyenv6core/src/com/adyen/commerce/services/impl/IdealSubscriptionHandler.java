d5dc404de66828263f0f9c78e040f2da467bedb5
PJaneta <p.janeta@reply.com>
2025-10-16 08:24:39 +0200

    AD-368 Tokenize LPM’s

--- adyenv6core/src/com/adyen/commerce/services/impl/IdealSubscriptionHandler.java
+++ adyenv6core/src/com/adyen/commerce/services/impl/IdealSubscriptionHandler.java
@@ -3,13 +3,13 @@ package com.adyen.commerce.services.impl;
 import com.adyen.model.checkout.CheckoutPaymentMethod;
 import com.adyen.model.checkout.IdealDetails;
 import com.adyen.model.checkout.PaymentRequest;
+import com.adyen.model.checkout.SepaDirectDebitDetails;
 import com.adyen.v6.enums.RecurringContractMode;
 import de.hybris.platform.commercefacades.order.data.CartData;
 import de.hybris.platform.core.model.user.CustomerModel;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.Arrays;
-import java.util.Objects;
 
 public class IdealSubscriptionHandler implements PaymentMethodHandler {
     @Override
@@ -22,17 +22,13 @@ public class IdealSubscriptionHandler implements PaymentMethodHandler {
     public void updatePaymentRequest(PaymentRequest paymentRequest, CartData cartData, RecurringContractMode recurringContractMode, CustomerModel customerModel, Boolean is3DS2Allowed, Boolean guestUserTokenizationEnabled) {
         if (StringUtils.isNotEmpty(cartData.getAdyenSelectedReference())) {
 
-            IdealDetails idealDetails;
 
-            if (Objects.isNull(paymentRequest.getPaymentMethod()) || Objects.isNull(paymentRequest.getPaymentMethod().getIdealDetails())) {
-                idealDetails = new IdealDetails();
-            } else {
-                idealDetails = paymentRequest.getPaymentMethod().getIdealDetails();
-            }
+            SepaDirectDebitDetails sepaDirectDebitDetails = new SepaDirectDebitDetails();
 
-            idealDetails.setStoredPaymentMethodId(cartData.getAdyenSelectedReference());
+
+            sepaDirectDebitDetails.setStoredPaymentMethodId(cartData.getAdyenSelectedReference());
             CheckoutPaymentMethod checkoutPaymentMethod = new CheckoutPaymentMethod();
-            checkoutPaymentMethod.setActualInstance(idealDetails);
+            checkoutPaymentMethod.setActualInstance(sepaDirectDebitDetails);
             paymentRequest.setPaymentMethod(checkoutPaymentMethod);
 
             paymentRequest.setRecurringProcessingModel(PaymentRequest.RecurringProcessingModelEnum.SUBSCRIPTION);
