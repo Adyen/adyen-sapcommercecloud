4ee00d5786a9538bf1989c8a4a9811d4ea696d40
PJaneta <p.janeta@reply.com>
2025-12-04 10:10:50 +0100

    AD-480 Add translation to Delivery Cost

---
+++ adyenv6core/src/com/adyen/commerce/util/LocalizationUtil.java
@@ -0,0 +1,20 @@
+package com.adyen.commerce.util;
+
+import de.hybris.platform.util.localization.Localization;
+import org.springframework.util.Assert;
+
+public class LocalizationUtil {
+
+    public static String getLocalizedStringOrDefault(final String key, final String defaultValue) {
+        Assert.notNull(key, "key must not be null");
+        Assert.notNull(defaultValue, "defaultValue must not be null");
+
+        String localizedString = Localization.getLocalizedString(key);
+
+        if (key.equals(localizedString)) {
+            return defaultValue;
+        }
+        return localizedString;
+    }
+
+}
