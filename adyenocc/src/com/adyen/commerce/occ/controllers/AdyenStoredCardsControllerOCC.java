9a28f0874a687985badd1666f78f11b5f248c9ba
Jakub Hasikowski <40691777+hasiq@users.noreply.github.com>
2026-03-09 12:12:17 +0100

    AD-439-b: Fixed models for occ api

--- adyenocc/src/com/adyen/commerce/occ/controllers/AdyenStoredCardsControllerOCC.java
+++ adyenocc/src/com/adyen/commerce/occ/controllers/AdyenStoredCardsControllerOCC.java
@@ -1,6 +1,5 @@
 package com.adyen.commerce.occ.controllers;
 
-import com.adyen.model.recurring.RecurringDetail;
 import com.adyen.service.exception.ApiException;
 import com.adyen.v6.facades.AdyenCheckoutFacade;
 import de.hybris.platform.commercefacades.customer.CustomerFacade;
@@ -19,6 +18,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.adyen.model.checkout.StoredPaymentMethodResource;
 
 import java.io.IOException;
 import java.util.List;
@@ -41,9 +41,9 @@ public class AdyenStoredCardsControllerOCC {
     @Operation(operationId = "getStoredCards", summary = "Returns stored card for user", description =
             "Returns stored card for use")
     @ApiBaseSiteIdAndUserIdParam
-    public ResponseEntity<List<RecurringDetail>> getStoredCards() throws IOException, ApiException {
+    public ResponseEntity<List<StoredPaymentMethodResource>> getStoredCards() throws IOException, ApiException {
         CustomerData currentCustomer = customerFacade.getCurrentCustomer();
-        List<RecurringDetail> storedCards = adyenCheckoutFacade.getAdyenPaymentService().getStoredCards(currentCustomer.getCustomerId());
+        List<StoredPaymentMethodResource> storedCards = adyenCheckoutFacade.getAdyenPaymentService().getStoredCards(currentCustomer.getCustomerId());
         return ResponseEntity.ok(storedCards);
     }
 
