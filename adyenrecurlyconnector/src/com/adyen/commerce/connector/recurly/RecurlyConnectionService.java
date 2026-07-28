23d2d7ca3b4303578b81cc6359255f623197dd8c
l.gasiorowski <l.gasiorowski@reply.com>
2026-07-28 16:10:36 +0200

    AD-651 Remove legacy Recurly connector and add BillingAddress DTO for token import operations.

--- adyenrecurlyconnector/src/com/adyen/commerce/connector/recurly/RecurlyConnectionService.java
+++
@@ -1,8 +0,0 @@
-package com.adyen.commerce.connector.recurly;
-
-import com.adyen.commerce.connector.exception.ConnectorNotConfiguredException;
-import com.adyen.commerce.connector.exception.RetryableBillingException;
-
-public interface RecurlyConnectionService {
-    boolean testConnection() throws RetryableBillingException, ConnectorNotConfiguredException;
-}
